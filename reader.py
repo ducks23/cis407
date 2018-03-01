73a4c435745fe0dc7e742cadb66725b94996394a
Jesse Leoanrd <jesse1517@gmail.com>
2018-02-28 21:15:34 -0800

    figured out finally how to iterate through my json file. yeesh

--- reader.py
+++ reader.py
@@ -1,6 +1,54 @@
 import json
+from collections import Counter
 
-with open('dat.json', 'r') as f:
-	line = f.readline()
-	tweet = json.loads(line)
-	print(json.dumps(tweet, indent=4))
+i = 0;
+fname = 'dat.json'
+with open(fname, 'r') as f:
+	for line in f:
+		i+=1
+	print(i);
+	
+with open(fname, 'r') as g:
+	for x in range(i):
+		line = g.readline()
+		a = g.readline()
+		tweet = json.loads(line)
+		print(x)
+		print(json.dumps(tweet, indent=4)) 
+
+import re
+ 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+tweet = 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
+print(preprocess(tweet))
